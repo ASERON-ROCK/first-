86cbb7171d7b4408e0aad719008b191cba2d5b8e
ASERON <amartyabera2019@gmail.com>
2023-03-03 04:40:49 +0530

    Initian commite

---
+++ first.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf(" eneter the numebr ");
+    scanf("%d", &n);
+    int j = 1;
+    int flag = 0;
+    for (j = 2; j <= n / 2; j++)
+    {
+        if (n % j == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("number is prime");
+    }
+    else
+    {
+        printf("numbr is not prime");
+    }
+}
